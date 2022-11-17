80212ca7ac8b58f06a4130e482914856cf9309b8
Taehwan Kim <obtusa07@gmail.com>
2022-11-17 09:46:40 +0900

    Initial Commit

---
+++ CICD-Practice/CICD_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_PracticeApp.swift
+//  CICD-Practice
+//
+//  Created by Taehwan Kim on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct CICD_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD-Practice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CICD-Practice
+//
+//  Created by Taehwan Kim on 2022/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
